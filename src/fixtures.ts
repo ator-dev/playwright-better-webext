96119a3bd3edc2af4012ff3182be2e71671335a4
ator-dev <dominic.codedeveloper@gmail.com>
2025-06-04 01:01:50 +0100

    Restyle fixtures.ts

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -1,36 +1,29 @@
 import { withExtension } from "./factory";
 import { test as base, expect } from "@playwright/test";
 
-export const createFixture = (extPaths: string | string[]) => {
+export const createFixture = (extensionPaths: string | string[]) => {
   const test = base.extend<Record<never, never>>({
-    browser: [
-      async ({ playwright, browserName }, use) => {
-        await use(
-          await withExtension(playwright[browserName], extPaths).launch(),
-        );
-      },
-      { scope: "worker", timeout: 0 },
-    ],
-    context: [
-      async ({ context, playwright, browserName }, use) => {
-        if (browserName === "chromium") {
-          const browserType = withExtension(playwright[browserName], extPaths);
-          const newContext = await browserType.launchPersistentContext("", {
-            channel: "chromium",
-            headless: true,
-          });
-          await use(newContext);
-          await context.close();
-        } else {
-          await use(context);
-          await context.close();
-        }
-      },
-      { scope: "test" },
-    ],
+    browser: [async ({ playwright, browserName }, use) => {
+      await use(
+        await withExtension(playwright[browserName], extensionPaths).launch(),
+      );
+    }, { scope: "worker", timeout: 0 }],
+
+    context: [async ({ context, playwright, browserName }, use) => {
+      if (browserName === "chromium") {
+        const browserType = withExtension(playwright[browserName], extensionPaths);
+        const newContext = await browserType.launchPersistentContext("", {
+          channel: "chromium",
+          headless: true,
+        });
+        await use(newContext);
+        await context.close();
+      } else {
+        await use(context);
+        await context.close();
+      }
+    }, { scope: "test" }],
   });
-  return {
-    test,
-    expect,
-  };
+
+  return { test, expect };
 };
